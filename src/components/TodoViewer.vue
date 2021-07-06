368d707231ad0ec728b15c4b1ecef443a4d38755
yakzi <zieba.jakub.97@gmail.com>
2021-07-06 18:47:25 +0200

    ADD TodoViewer component

---
+++ src/components/TodoViewer.vue
@@ -0,0 +1,16 @@
+<template> 
+<div v-for="todo in todos">
+    <h3> {{ todo.text }}</h3>
+
+</div>
+
+</template>
+
+<script>
+    export default {
+        name: 'TodoViewer',
+        props: {
+            todos: Array
+        }
+    }
+</script>
