15a5537383e84d3f186950a03a6e4b3662255c71
yakzi <zieba.jakub.97@gmail.com>
2021-07-06 18:58:19 +0200

    ADD single todo item component

---
+++ src/components/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="todo">
+    <h3>{{ todo.text }}</h3>
+    <p>{{ todo.priority }}</p>
+</div>
+
+</template>
+
+<script> 
+export default{
+    name: 'Todo',
+    props: {
+        todo: Object
+    }
+}
+</script>
+
+<style scope> 
+
+.todo {
+    background: lightblue;
+    text-align: left;
+    padding-left: 5px;
+}
+
+.todo p{
+    color: lightcoral;
+}
+
+</style>
