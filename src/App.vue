358c0fa64a7a7a7f60dbcbd7a084c710d245d860
yakzi <zieba.jakub.97@gmail.com>
2021-07-06 18:57:32 +0200

    MODIFY add sample data to test TodoViewer component, MODIFY minor code refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
 <div class="container">
   <Header title="VueDo - ToDo List made with Vue.JS"/>
+  <TodoViewer :todos="todos" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import TodoViewer from './components/TodoViewer.vue' 
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    TodoViewer
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  created() {
+    this.todos = [
+      {
+        id: 1,
+        text: 'Buy new car',
+        priority: 'High'
+      },
+      {
+        id: 2,
+        text: 'Sell old phone',
+        priority: 'Medium'
+      },
+      {
+        id: 3,
+        text: 'Pick up new doors',
+        priority: 'High'
+      }
+    ]
   }
 }
 </script>
