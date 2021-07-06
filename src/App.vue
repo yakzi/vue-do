806074e41ac91751f89b6d75c20c50a1c33a2c05
yakzi <zieba.jakub.97@gmail.com>
2021-07-06 18:03:22 +0200

    INIT commit, ADD Header compontent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <Header/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>VueDo - ToDo List</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+header{
+    justify-content: space-between;
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+}
+</style>
