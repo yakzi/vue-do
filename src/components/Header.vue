0b913a8ee02d587ca470345b3ea0a491a4f189e8
yakzi <zieba.jakub.97@gmail.com>
2021-07-06 18:29:14 +0200

    FIXED typo in header component causing button init fail

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,24 @@
 <template>
     <header>
-        <h1>VueDo - ToDo List</h1>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green"/>
     </header>
 </template>
 
 <script>
+import Button from './Button'
+
 export default {
     name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+    Button
 }
+}
+
+
 </script>
 
 <style scoped>
