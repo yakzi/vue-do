aa52363d44c0eed0223efdcb488df0dc112519bf
yakzi <zieba.jakub.97@gmail.com>
2021-07-06 19:34:08 +0200

    MODIFY add props and methods to button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,20 @@
 <template> 
-    <button>Add Task</button>
+    <button @click="onClick()"
+            :style="{ background: color }" 
+            class="btn">{{ text }}</button>
 </template>
 
 <script> 
 export default{
     name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        OnClick() {
+            console.log('clicked');
+        }
+    }
 }
 </script>
